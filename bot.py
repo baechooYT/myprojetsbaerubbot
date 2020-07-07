8fae48e6a5eeaadd09b5e1579af516c7a9f35b6e
yourfancingto <66154521+yourfancingto@users.noreply.github.com>
2020-07-07 17:24:19 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ async def on_ready():
     print("작동중")
     game = discord.Game("'배룹아 도움말' 으로 도움말 확인하기")
     await  client.change_presence(status=discord.Status.online, activity=game)
+    client = commands.Bot(command_prefix = 'b!') 
 
 
 @client.event
@@ -42,6 +43,13 @@ async def on_message(message):
             await message.channel.send(server.name)
     if message.content.startswith("b!초대"):
         await  message.channel.send ("배룹봇 초대하기 : https://bit.ly/2VTcNs1")
+
+
+
+@client.command()
+async def 청소(ctx, amount):
+	await ctx.client.purge(limit = amount)
+
         
 
 access_token = os.environ["BOT_TOKEN"]
